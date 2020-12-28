667c1db70993fc81ef11c6471d3f54e60bd2161e
Chris Baum <christopher.baum@valpo.edu>
2020-12-28 16:53:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,8 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" #return Fizz
-  elsif int % 5 == 0
+  end
+  if int % 5 == 0
     "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
-  else
   end
 end
